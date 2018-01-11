05c7cc554c8e6422d453e6ef38dbe179efcc2a23
Yang LI <zjzsliyang@gmail.com>
2018-01-11 10:12:42 +0800

    fix bugs

--- Express/src/main/java/com/external/express/ExpressApplication.java
+++ Express/src/main/java/com/external/express/ExpressApplication.java
@@ -2,10 +2,12 @@ package com.external.express;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author xuantang
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ExpressApplication {
 
